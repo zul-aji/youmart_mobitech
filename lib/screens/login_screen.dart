e808ee53097f2e4ce7c5bf04fc774821fdfe3c78
Zulfiqar Rahman Aji <zulfiqar.aji@gmail.com>
2022-04-21 16:07:36 +0800

    Added Temporary Home Screen
    Clicking the login button on the Login Screen will redirect to the temporary login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youmart_mobitech/screens/home_screen.dart';
 import 'package:youmart_mobitech/screens/registration_screen.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -60,7 +61,10 @@ class _LoginScreenState extends State<LoginScreen> {
       child: MaterialButton(
         padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
         minWidth: MediaQuery.of(context).size.width,
-        onPressed: () {},
+        onPressed: () {
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => HomeScreen()));
+        },
         child: Text(
           "Login",
           textAlign: TextAlign.center,
