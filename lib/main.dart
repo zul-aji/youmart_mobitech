cd5b4399400ad2869cd5580aea675641c95b6482
*gilbertk27* <64368640+gilbertk27@users.noreply.github.com>
2022-04-24 13:18:22 +0800

    add firebase to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:youmart_mobitech/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
