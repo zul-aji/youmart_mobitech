d0380a5f04555f25baf065b10680626cbf03c328
gilbertk27 <64368640+gilbertk27@users.noreply.github.com>
2022-05-10 22:11:22 +0800

    successfully add & upload images to firebase storage

--- lib/screens/home/components/body/add_item.dart
+++ lib/screens/home/components/body/add_item.dart
@@ -1,6 +1,15 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import '../../../../constants.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:youmart_mobitech/widget/button_widget.dart';
+import 'package:youmart_mobitech/api/firebase_api.dart';
+import 'package:path/path.dart';
+
 
 class AddItem extends StatefulWidget {
   AddItem({Key? key}) : super(key: key);
@@ -10,12 +19,16 @@ class AddItem extends StatefulWidget {
 }
 
 class _AddItemState extends State<AddItem> {
+  File? file;
+  UploadTask? task;
   @override
+
   Widget build(BuildContext context) {
     //Controllers
     final itemNameController = TextEditingController();
     final itemPriceController = TextEditingController();
     final itemCategoryController = TextEditingController();
+    final fileName = file != null ? basename(file!.path) : 'No File Selected';
 
     // item name field
     final itemNameField = TextFormField(
@@ -113,6 +126,7 @@ class _AddItemState extends State<AddItem> {
     );
 
     //Upload image button
+
     final uploadImageButton = Material(
       elevation: 5,
       borderRadius: BorderRadius.circular(30),
@@ -120,7 +134,7 @@ class _AddItemState extends State<AddItem> {
       child: MaterialButton(
         padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
         minWidth: MediaQuery.of(context).size.width,
-        onPressed: () {},
+        onPressed: selectFile,
         child: const Text(
           "Upload Image",
           textAlign: TextAlign.center,
@@ -138,7 +152,7 @@ class _AddItemState extends State<AddItem> {
       child: MaterialButton(
         padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
         minWidth: MediaQuery.of(context).size.width,
-        onPressed: () {},
+        onPressed: uploadFile,
         child: const Text(
           "Upload Item",
           textAlign: TextAlign.center,
@@ -170,10 +184,62 @@ class _AddItemState extends State<AddItem> {
           itemCategoryField,
           const SizedBox(height: 25),
           uploadImageButton,
-          const SizedBox(height: 50),
+          Text(
+            fileName,
+            style: TextStyle(
+                fontSize: 16, fontWeight: FontWeight.w500
+            ),
+          ),
+          const SizedBox(height: 20),
           uploadItemButton,
+          task != null ? buildUploadStatus(task!) : Container(),
         ],
       ),
     );
   }
+  Future selectFile() async {
+    final result = await FilePicker.platform.pickFiles(allowMultiple: false);
+
+    if (result == null) return;
+    final path = result.files.single.path!;
+
+    setState(() => file = File(path));
+  }
+
+  Future uploadFile() async {
+    if (file == null) return;
+
+    final fileName = basename(file!.path);
+    final destination = 'itemimage/$fileName';
+
+    task = FirebaseApi.uploadFile(destination, file!);
+    setState(() {});
+
+    if (task == null) return;
+
+    final snapshot = await task!.whenComplete(() {});
+    final urlDownload = await snapshot.ref.getDownloadURL();
+
+    print('Download-Link: $urlDownload');
+  }
+
+  Widget buildUploadStatus(UploadTask task) => StreamBuilder<TaskSnapshot>(
+    stream: task.snapshotEvents,
+    builder: (context, snapshot) {
+      if (snapshot.hasData) {
+        final snap = snapshot.data!;
+        final progress = snap.bytesTransferred / snap.totalBytes;
+        final percentage = (progress * 100).toStringAsFixed(2);
+
+        return Text(
+          '$percentage %',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        );
+      } else {
+        return Container();
+      }
+    },
+  );
+
 }
+
